63fbdfed4145ae76399457455e4469c772c233fd
Jaime-Sanz <118962371+Jaime-Sanz@users.noreply.github.com>
2024-02-23 15:11:09 -0800

    pushing to main
    switching computers updating version

--- src/index.js
+++ src/index.js
@@ -1,4 +1,18 @@
-// /your-project/src/index.js
 import './styles.css';
 
-console.log('Hello, webpack!');
+const API_KEY = '0ade93a78b6c4f04b7d23501240502';
+
+
+function getWeather(cityName){
+    const URL = `http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${cityName}&days=3`;
+
+    fetch(URL)
+        .then(response => response.json())
+        .then(data => {
+           console.log(data);
+        })
+    
+}
+
+
+getWeather("Bell Gardens");
